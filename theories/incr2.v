7d63d6cee66c9efa86c0cad2827a988aec0fddc7
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2020-03-26 15:35:42 +0100

    incr2.v: some cleanup

--- theories/incr2.v
+++ theories/incr2.v
@@ -25,28 +25,17 @@ Section S.
   Proof.
     iIntros (φ) "#Hi Hφ". iLöb as "IH".
     wp_lam. wp_bind (_ ||| _)%E. wp_pures.
-    iApply (wp_par (fun _ => ⌜True⌝)%I (fun _ => ⌜True⌝)%I).
-    { wp_bind (!#ℓ)%E. iInv N as ">H" "Hclose".
-      iDestruct "H" as (x) "[Hℓ %]". wp_load.
-      iMod ("Hclose" with "[Hℓ]").
-      { iNext. iExists x. iFrame; eauto. }
+    (* specification for the two threads *)
+    iAssert (WP #ℓ <- ! #ℓ + #2 {{ _, True }})%I with "[Hi]" as "Ht".
+    { wp_bind (!#ℓ)%E. iInv N as (x) ">[Hℓ %]" "Hclose". wp_load.
+      iMod ("Hclose" with "[Hℓ]"). now eauto.
       iModIntro. wp_pures.
-      iInv N as ">H" "Hclose".
-      iDestruct "H" as (x') "[Hℓ %]". wp_store.
-      iMod ("Hclose" with "[Hℓ]").
-      { iNext. iExists (x+2). iFrame. eauto using Zeven_add_2. }
-      eauto. }
-    { wp_bind (!#ℓ)%E. iInv N as ">H" "Hclose".
-      iDestruct "H" as (x) "[Hℓ %]". wp_load.
-      iMod ("Hclose" with "[Hℓ]").
-      { iNext. iExists x. iFrame; eauto. }
-      iModIntro. wp_pures.
-      iInv N as ">H" "Hclose".
-      iDestruct "H" as (x') "[Hℓ %]". wp_store.
-      iMod ("Hclose" with "[Hℓ]").
-      { iNext. iExists (x+2). iFrame. eauto using Zeven_add_2. }
-      eauto. }
-    iIntros (? ?) "_". iNext. wp_pures. iApply "IH". eauto.
+      iInv N as (x') ">[Hℓ %]" "Hclose". wp_store.
+      iMod ("Hclose" with "[Hℓ]"); eauto.
+      iNext. iExists (x+2). iFrame. eauto using Zeven_add_2. }
+    iApply (wp_par (fun _ => ⌜True⌝)%I (fun _ => ⌜True⌝)%I);
+      [ iApply "Ht" .. |].
+    iIntros. iNext. wp_pures. iApply "IH". eauto.
   Qed.
 End S.
 
@@ -73,54 +62,56 @@ Section S.
     pose proof (@incr2_spec Σ) as S.
 
     pose proof (wp_invariance Σ heap_lang NotStuck) as L. cbn in L.
-    specialize (L (incr2 #ℓ) σ1 es σ2).
-
-    specialize (L (φ σ2)). (* ? *)
+    specialize (L (incr2 #ℓ) σ1 es σ2 (φ σ2)).
     eapply L; eauto. intros Hinv κs. clear L.
 
+    (* Allocate heap resources corresponding to (heap σ1) *)
     iDestruct (gen_heap_init (delete ℓ (heap σ1))) as ">H".
     iDestruct "H" as (Hheap) "Hσ1".
+    (* Also allocate the prophecy-related resources, that appear in the standard
+       state interpretation in heapG_irisG (this is needed for things to match
+       later on). *)
     iDestruct (proph_map_init κs (used_proph_id σ1)) as ">H".
     iDestruct "H" as (Hproph) "Hpr1".
 
-    set (HeapΣ := HeapG Σ Hinv Hheap Hproph).
     (* /!\ do not use 'pose proof', as [heapG_invG HeapΣ] needs to
-       stay convertible to Hinv *)
+       stay convertible to Hinv for things to typecheck later on. *)
+    set (HeapΣ := HeapG Σ Hinv Hheap Hproph).
 
+    (* Add ℓ back to the heap, so that we get a corresponding pointsto *)
     destruct Hσ1 as [v1 [Hσ1 E1]].
     iMod (gen_heap_alloc (delete ℓ (heap σ1)) ℓ (#v1) with "Hσ1")
       as "(Hσ1 & Hℓ & ?)".
     by rewrite lookup_delete.
 
+    (* Allocate the same invariant as in [incr2_spec], so that we can feed it to
+       [S] *)
     iPoseProof (@inv_alloc Σ Hinv N ⊤
-           (∃ (x:Z), ℓ ↦ #x ∗ ⌜Z.even x⌝)%I with "[Hℓ]") as "HI".
-    { iNext. iExists v1. eauto. }
-    iMod "HI". iDestruct "HI" as "#HI".
+      (∃ (x:Z), ℓ ↦ #x ∗ ⌜Z.even x⌝)%I with "[Hℓ]") as ">#HI".
+    by iExists v1; eauto.
 
     rewrite insert_delete insert_id //.
-
     specialize (S HeapΣ spawnG0 N ℓ (fun _ => True%I)).
-    iPoseProof (S with "[HI]") as "S". eauto.
+    iPoseProof (S with "HI") as "S".
 
     iModIntro.
 
-    (* /!\ this has to match the state_interp of heapG_irisG *)
+    (* /!\ this has to match exactly the state_interp of heapG_irisG *)
     iExists (fun σ 𝜅s _ =>
       gen_heap_ctx (heap σ) ∗ proph_map_ctx 𝜅s (used_proph_id σ))%I.
     iExists (fun _ => True%I).
     iSplitL "Hσ1 Hpr1"; [ by iFrame |].
-    iSplitL "S". { by iApply "S". }
-    iIntros "[Hheap ?]".
-    iExists (⊤ ∖ ↑N).
-    iInv N as ">H" "Hclose". iDestruct "H" as (x) "[Hℓ %]".
+    iSplitL "S". by iApply "S".
+    iIntros "[Hheap ?]". iExists (⊤ ∖ ↑N).
+    iInv N as (x) ">[Hℓ %]" "Hclose".
     iDestruct (gen_heap_valid with "Hheap Hℓ") as "%".
     iModIntro. iPureIntro. unfold φ. eauto.
   Qed.
 End S.
 
 Theorem invariant_adequacy' (ℓ : loc) (σ1 σ2 : state) (es: list expr) :
-    let φ := (fun (σ: state) => ∃ (x:Z),
-                heap σ !! ℓ = Some (#x) ∧ Z.even x) in
+    let φ := (fun (σ: state) =>
+              ∃ (x:Z), heap σ !! ℓ = Some (#x) ∧ Z.even x) in
     φ σ1 →
     rtc erased_step ([incr2 #ℓ], σ1) (es, σ2) →
     φ σ2.
